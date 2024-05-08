93d881142468d54816eb96ba9d617344f8598c79
Alex <AlexKozlovDeveloper@gmail.com>
2024-05-08 16:06:36 +0300

    Add IdentityDbContext

---
+++ UserMonitoringSystem/UserMonitoringSystem.Server/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace UserMonitoringSystem.Server
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext() : base()
+        {
+        
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        
+        }
+    }
+}
--- UserMonitoringSystem/UserMonitoringSystem.Server/Program.cs
+++ UserMonitoringSystem/UserMonitoringSystem.Server/Program.cs
@@ -1,6 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using UserMonitoringSystem.Server;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(connectionString)
+    );
+
+//builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
